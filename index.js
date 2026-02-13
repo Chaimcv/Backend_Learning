1a130e6dcc5d7a3c7cd80e73d1c735aad9c6e5c2
Chaimcv <chaithanyamcv@gmail.com>
2026-02-13 18:37:55 +0530

    connection established

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express=require("express");
 const app=express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const port=3000;
+const port=8000;
 require('dotenv').config();
 const url=process.env.MONGO_URL;
 console.log(url,"url");
 const DbConnection=require("./config/ConnectDB");
 const TeacherRouter = require("./Routes/TeacherRoute");
+const cors = require('cors');
+
+app.use(cors({ origin: ['http://localhost:3000'] }));
 
 
 
